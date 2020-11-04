31b0b3df43be4577e8d329fdb7c846564cc794fb
Babeluka7 <70277798+Babeluka7@users.noreply.github.com>
2020-11-04 20:01:13 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,49 @@
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showString("youtube.com/user/hvukui")
+    basic.showString("SUBSCRIBE")
+})
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 50; index++) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 50; index++) {
+        basic.showLeds(`
+            # . # . #
+            . # . # .
+            # . # . #
+            . # . # .
+            # . # . #
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
+basic.showArrow(ArrowNames.North)
+basic.showArrow(ArrowNames.NorthEast)
+basic.showArrow(ArrowNames.East)
+basic.showArrow(ArrowNames.SouthEast)
+basic.showArrow(ArrowNames.South)
+basic.showArrow(ArrowNames.SouthWest)
+basic.showArrow(ArrowNames.West)
+basic.showArrow(ArrowNames.NorthWest)
+basic.showArrow(ArrowNames.North)
